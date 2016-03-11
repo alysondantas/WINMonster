13be2ecff1aff27ad9d9b86a7167efb52a83a2e8
alysondantas <alyson.lipe@gmail.com>
2016-03-11 01:23:59 -0300

    update arvore

--- src/main/java/br/uefs/ecomp/winMonster/util/Arvore.java
+++ src/main/java/br/uefs/ecomp/winMonster/util/Arvore.java
@@ -56,26 +56,26 @@ public class Arvore {
 
 
 
-	public void inserir2(CelulaArvore node, int valor) {
+	/*	public void inserir2(CelulaArvore node, int valor) {
 		//Verifica se o valor a ser inserido é menor que o nodo corrente da árovre, se sim vai para subarvore esquerda
-		if (valor < node.valor) {
+		if (valor < node.getChave()) {
 			//Se tiver elemento no nodo esquerdo continua a busca
-			if (node.esquerda != null) {
-				inserir(node.esquerda, valor);
+			if (node.getEsquerda() != null) {
+				inserir2(node.getEsquerda(), valor);
 			} else {
 				//Se nodo esquerdo vazio insere o novo nodo aqui
 				System.out.println("  Inserindo " + valor + " a esquerda de " + node.valor);
-				node.esquerda = new No(valor);
+				node.getEsquerda() = new CelulaArvore(valor);
 			}
 			//Verifica se o valor a ser inserido é maior que o nodo corrente da árvore, se sim vai para subarvore direita
 		} else if (valor > node.valor) {
 			//Se tiver elemento no nodo direito continua a busca
 			if (node.direita != null) {
-				inserir(node.direita, valor);
+				inserir2(node.direita, valor);
 			} else {
 				//Se nodo direito vazio insere o novo nodo aqui
 				System.out.println("  Inserindo " + valor + " a direita de " + node.valor);
 				node.direita = new No(valor);
 			}
-		}
-	}
+		}*/
+}
