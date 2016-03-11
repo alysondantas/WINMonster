3c9e1a5200e06139531eb1f4fa2a9a72c3172e4b
alysondantas <alyson.lipe@gmail.com>
2016-03-10 22:44:13 -0300

    Alyson up

--- src/main/java/br/uefs/ecomp/winMonster/util/Arvore.java
+++ src/main/java/br/uefs/ecomp/winMonster/util/Arvore.java
@@ -3,4 +3,7 @@ package br.uefs.ecomp.winMonster.util;
 public class Arvore {
 	Celula raiz;
 	
+	public boolean estaVazia() {
+		return raiz==null;//retorna true se a primeira celula for nula
+	}
 }
