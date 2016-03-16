289441b8821abd1280751c9700201760e7e89bd2
brunomlima11 <brunomenezes253@gmail.com>
2016-03-16 19:33:29 -0300

    teste

--- src/main/java/br/uefs/ecomp/winMonster/controller/AdministradorController.java
+++ src/main/java/br/uefs/ecomp/winMonster/controller/AdministradorController.java
@@ -25,6 +25,34 @@ public class AdministradorController {
 		}
 		return linha; //retorna a String com todo o conteúdo do arquivo de texto
 	}
+	
+	public void lerArquivo2() throws IOException {
+		 Scanner ler = new Scanner(System.in);
+		 System.out.printf("Informe o nome de arquivo texto:\n");
+		    String nome = ler.nextLine();
+
+		    System.out.printf("\nConteúdo do arquivo texto:\n");
+		    try {
+		      FileReader arq = new FileReader(nome);
+		      BufferedReader lerArq = new BufferedReader(arq);
+
+		      String linha = lerArq.readLine(); // lê a primeira linha
+		// a variável "linha" recebe o valor "null" quando o processo
+		// de repetição atingir o final do arquivo texto
+		      while (linha != null) {
+		        System.out.printf("%s\n", linha);
+
+		        linha = lerArq.readLine(); // lê da segunda até a última linha
+		      }
+
+		      arq.close();
+		    } catch (IOException e) {
+		        System.err.printf("Erro na abertura do arquivo: %s.\n",
+		          e.getMessage());
+		    }
+
+		    System.out.println();
+		  }
 
 	public void escreverArquivo(String local) throws IOException { //método deixado aqui só para inspiração. Não vai ser usado do jeito que está descrito no momento
 		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(local)); //crio um novo objeto para escrita de arquivos e passo como parâmetro um novo objeto de escrita do arquivo no local especificado
--- src/main/java/br/uefs/ecomp/winMonster/main/main.java
+++ src/main/java/br/uefs/ecomp/winMonster/main/main.java
@@ -2,8 +2,20 @@ package br.uefs.ecomp.winMonster.main;
 
 import br.uefs.ecomp.winMonster.view.GUI;
 
+import java.io.IOException;
+
+import br.uefs.ecomp.winMonster.controller.*;
+
 public class main {
 	public static void main (String[] args) {
-		GUI gui = new GUI();
+		//GUI gui = new GUI();
+		AdministradorController controller = new AdministradorController();
+		try {
+			String str = controller.lerArquivo("C:\\Users\Megazord\Desktop\Anime\\temp.txt");
+			System.out.println(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
