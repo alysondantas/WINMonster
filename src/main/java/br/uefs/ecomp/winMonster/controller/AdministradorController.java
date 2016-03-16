edbee6a45d1a0bc9eded05e9020d5025417fc23f
brunomlima11 <brunomenezes253@gmail.com>
2016-03-16 20:26:59 -0300

    gerarPrioridade(String linha, Fila copia) e leitura de arquivos
    implementada a geração de uma fila cópia em gerarPrioridade() e
    corrigida a leitura de arquivos para ficar quase funcionando...

--- src/main/java/br/uefs/ecomp/winMonster/controller/AdministradorController.java
+++ src/main/java/br/uefs/ecomp/winMonster/controller/AdministradorController.java
@@ -16,18 +16,17 @@ import br.uefs.ecomp.winMonster.util.MeuIterador;
 
 public class AdministradorController {
 	public String lerArquivo(String local) throws IOException { //método que lê um arquivo de texto e converte todo o seu conteúdo para uma String
-		BufferedReader buffRead = new BufferedReader(new FileReader(local)); //crio um novo objeto para leitura de arquivos e passo como parâmetro a leitura do arquivo no local especificado
-		String linha = ""; //crio uma string auxiliar e a inicializo como vazia
+		FileReader arq = new FileReader(local);
+		BufferedReader buffRead = new BufferedReader(arq); //crio um novo objeto para leitura de arquivos e passo como parâmetro a leitura do arquivo no local especificado
+		String linha = buffRead.readLine(); //crio uma string auxiliar e já armazeno a primeira linha do arquivo
+		String c = ""; //crio uma string auxiliar para armazenar o conteúdo da string
+		
 		while(linha != null) { //enquanto não chegar no fim do arquivo
+			c = c + linha; //salvo o a letra da string na posição atual
 			linha = buffRead.readLine(); //salvo a linha atual do arquivo na string
-			int i;
-			JOptionPane.showMessageDialog(null, linha.length());
-			for(i = 0; i < linha.length(); i++) { //percorro letra por letra do arquivo, até o seu fim
-				String c = ""; //crio uma string auxiliar para armazenar o caractere da string
-				c = c + linha.charAt(i); //salvo o a letra da string na posição atual
-			}
-		}
-		return linha; //retorna a String com todo o conteúdo do arquivo de texto
+		} arq.close();
+		
+		return c; //retorna a String com todo o conteúdo do arquivo de texto
 	}
 	
 	public void lerArquivo2() throws IOException {
@@ -66,8 +65,12 @@ public class AdministradorController {
 		buffWrite.close(); //fecho o arquivo aberto
 	}
 	
-	public Fila gerarPrioridade(String linha) throws IOException { //método para gerar uma fila de prioridade a partir de uma String recebida
+	public Fila gerarPrioridade(String linha, Fila copia) throws IOException { //método para gerar uma fila de prioridade a partir de uma String recebida
 		Fila fila = new Fila(); //crio uma fila para salvar a prioridade
+		boolean copiaNula = false; //variável para verificar se é necesário criar uma fila cópia. Caso ela continue como false, vai ser gerada uma fila cópia igual a fila a ser retornada
+		if(copia == null) { //caso a cópia passada seja nula, marque a variável como true
+			copiaNula = true;
+		}
 		while(linha != "") { //enquanto a string não estiver vazia
 			int cont = 0; //variável para armazenar quantas vezes a letra se repete na string
 			String aux; //string auxiliar para ajudar nas operações
@@ -78,6 +81,8 @@ public class AdministradorController {
 				}
 			}
 			fila.inserir(cont, linha.substring(0, 1)); //crio uma string somente com a primeira letra de linha e insiro ela na fila, passando também o número de vezes que a letra se repete como chave
+			if(copiaNula == false) //caso a cópia não seja nula, preencha a fila cópia
+				copia.inserir(cont, linha.substring(0, 1)); //preencho a cópia com o mesmo conteúdo da fila original
 			aux = Character.toString(linha.charAt(0)); //atribui agora a primeira letra da string ao valor de aux
 			linha = linha.replace(aux, ""); //remove todas as ocorrências da primeira letra na string
 		} //repete o ciclo
