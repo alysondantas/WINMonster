dcf6fa8a28dd96eec8c63824f5812a967d21d415
alysondantas <alyson.lipe@gmail.com>
2016-03-19 14:07:04 -0300

    Classe Binario do Caractere

---
+++ src/main/java/br/uefs/ecomp/winMonster/model/BinarioDoCaractere.java
@@ -0,0 +1,30 @@
+package br.uefs.ecomp.winMonster.model;
+
+public class BinarioDoCaractere {
+	private String binario;
+	private boolean verificador;
+	/**
+	 * @return the binario
+	 */
+	public String getBinario() {
+		return binario;
+	}
+	/**
+	 * @param binario the binario to set
+	 */
+	public void setBinario(String binario) {
+		this.binario = binario;
+	}
+	/**
+	 * @return the verificador
+	 */
+	public boolean isVerificador() {
+		return verificador;
+	}
+	/**
+	 * @param verificador the verificador to set
+	 */
+	public void setVerificador(boolean verificador) {
+		this.verificador = verificador;
+	}
+}
