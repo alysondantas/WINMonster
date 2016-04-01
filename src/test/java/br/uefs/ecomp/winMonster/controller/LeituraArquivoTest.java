2a8a5b7944900f866fb77f1aa28142978eea2bb2
alysondantas <alyson.lipe@gmail.com>
2016-03-31 21:18:20 -0300

    Leitura de Arquivo Teste funcionando 100%

--- src/test/java/br/uefs/ecomp/winMonster/controller/LeituraArquivoTest.java
+++ src/test/java/br/uefs/ecomp/winMonster/controller/LeituraArquivoTest.java
@@ -15,15 +15,42 @@ public class LeituraArquivoTest {
 	
 	@Test
 	public void testLerArquivoSucesso() {
-		String local = "";
+		String local = "src\\test\\java\\br\\uefs\\ecomp\\winMonster\\util\\teste.txt";
 		String c = null;
 		try {
-			controller.lerArquivo(local);
+			c=controller.lerArquivo(local);
 		} catch (IOException e) {
 			fail();
 		}
 		
 		assertEquals("agua", c);
+		
+	}
+	
+	@Test
+	public void testLerArquivoInexistente() {
+		String local = "src\\test\\java\\br\\uefs\\ecomp\\winMonster\\util\\teste3.txt";
+		String c = null;
+		try {
+			c=controller.lerArquivo(local);
+		} catch (IOException e) {
+			assertTrue(true);
+		}
+		
+		assertEquals(null, c);	
+	}
+	
+	@Test
+	public void testLerArquivoGrandeSucesso() {
+		String local = "src\\test\\java\\br\\uefs\\ecomp\\winMonster\\util\\teste2.txt";
+		String c = null;
+		try {
+			c=controller.lerArquivo(local);
+		} catch (IOException e) {
+			fail();
+		}
+		
+		assertEquals("agua azul com chileno velho e arquivo com string meio grande aqui... Vai que não funciona quando o arquivo tem mais de 10 palvras, numeros como 12345 e outros caracteres como %$#*&¨@!? vai que né?", c);
 		
 	}
 }
